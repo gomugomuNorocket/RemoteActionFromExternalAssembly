6fab895fce1731f5bdc31e0359e8de4ab77caa1a
gomugomuNorocket <odysseus.boras@gmail.com>
2019-02-17 14:52:30 +0200

    final

---
+++ Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        protected static readonly log4net.ILog log = log4net.LogManager.GetLogger("Program");
+
+        static void Main(string[] args)
+        {
+            log.Info("Starting...Press Z to exit.");
+            ///Initalize Manager
+            ClientManager.Initalize();
+            ///Starting the client connection
+            HostManager.Start();           
+
+            while (true)
+            {
+                ///Exit for closing
+                string input = Console.ReadLine();
+                if (input == "Exit") { 
+                    break;
+                }
+            }
+            ///Stoping the client connection
+            HostManager.Stop();
+
+            ActionScriptManager.Dispose();
+
+            log.Info("Closing...");
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+
+            var hubConfiguration = new HubConfiguration()
+            {
+                EnableDetailedErrors = true,
+                EnableJSONP = true,
+                EnableJavaScriptProxies = true
+            };
+
+            app.MapSignalR(hubConfiguration);
+        }
+    }
+}
